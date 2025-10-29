99de4a0663881e86bc7cb8a8617c8e094640f95e
talbmax10 <talbmax10@gmail.com>
2025-10-29 16:47:28 +0000

    Updates

---
+++ precache-manifest.04006762d7113b056d5d66da61f2626f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ea95c66f02c98ecf68590fe0d72db40",
+    "url": "/ts-excel-compare/index.html"
+  },
+  {
+    "revision": "715a8b32f3ab49667bc6",
+    "url": "/ts-excel-compare/static/css/2.4bcb6717.chunk.css"
+  },
+  {
+    "revision": "cb8b81b66f17a9e70dfe",
+    "url": "/ts-excel-compare/static/css/main.8dea736c.chunk.css"
+  },
+  {
+    "revision": "715a8b32f3ab49667bc6",
+    "url": "/ts-excel-compare/static/js/2.cd4ea1d1.chunk.js"
+  },
+  {
+    "revision": "d317bcda24c81ace74b97e956e440b7a",
+    "url": "/ts-excel-compare/static/js/2.cd4ea1d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb8b81b66f17a9e70dfe",
+    "url": "/ts-excel-compare/static/js/main.0527eab7.chunk.js"
+  },
+  {
+    "revision": "382ceee8383e6e87641e",
+    "url": "/ts-excel-compare/static/js/runtime-main.52d35c72.js"
+  }
+]);
